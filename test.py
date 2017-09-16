7b3d38fb2a83337558bd74b00eb911d42b464bfa
allicsander <allicsander@example.com>
2017-09-16 10:30:42 +0300

    test.py

--- test.py
+++ test.py
@@ -12,6 +12,16 @@ print('Masha' in my_names)
 del my_names[:2]
 print(my_names)
 
+is_banned = True
+if not not is_banned:
+	print("chat goes on")
+
+x=0
+while x<10:
+	print(x+1)
+	x +=3
+
+
 # print(a + ' ' + b + '!')
 
 # print(type(c))
